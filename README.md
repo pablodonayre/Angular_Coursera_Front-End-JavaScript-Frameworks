9be125f0cd1993a4c067016f044a61561eb2d035
Jader <swilor29@gmail.com>
2020-10-27 11:08:35 -0500

    Exercise (Instructions): Angular Components Part 1

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# ConFusion
+# Semana1
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 6.2.1.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 10.1.1.
 
 ## Development server
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { FlexLayoutModule } from '@angular/flex-layout';
-import 'hammerjs';
-
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from "@angular/material/list";
+import { MenuComponent } from './components/menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule,
-    MatToolbarModule,
-    FlexLayoutModule
+    AppRoutingModule,
+    BrowserAnimationsModule, MatToolbarModule, MatIconModule, MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
