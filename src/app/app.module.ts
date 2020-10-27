5cb82f3a24c883957443625defa5fc2f6799c7da
Juan Pablo <pablodonayre@gmail.com>
2020-10-26 21:34:23 -0500

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import 'hammerjs';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
